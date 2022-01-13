2c69f355b227be24a03e6f7fce814fc3605b126d
marianob85 <marianob85@gmail.com>
2022-01-13 22:29:33 +0100

    Python 3

--- Generate.py
+++ Generate.py
@@ -2,7 +2,21 @@ from Kingofsat import Kingofsat
 from pprint import pprint
 
 if __name__ == '__main__':
-    kingOfSat = Kingofsat([130])
+
+    #output.write("%s:%04X|%s|%s|%s\n" % (caid, int(sid), name, channel, satellite))
+
+    providers = {
+        "Polsat": ['1803', '1813', '1861'],
+        "NC\+": ['0B01', '1884', '0100'],
+        "Platforma Canal\+": ['0100'],
+        "Telewizja na kartę":['0B00'],
+        'Nova': ['0500'],
+        'ORF': ['0648', '0650', '0D95', '0D98'],
+        'Sky': ['098D'],
+        'HD \+':['1830', '1843', '1860', '186A']
+    }
+
+    kingOfSat = Kingofsat([130, 192])
     for _, orbitaldata in kingOfSat.getServices().items():
         for data in orbitaldata:
-            pprint(vars(data))
+            pass
